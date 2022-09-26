00735442af473f30257699d5bcc031ede24b3cf3
Tim <github@timgreen.ws>
2022-09-26 16:52:08 +1000

    added function for covid data card

---
+++ src/components/shared/DataCard/DataCard.js
@@ -0,0 +1,10 @@
+import "./DataCard.css";
+
+export default function DataCard({title, data}){
+    return(
+        <div className="DataCard">
+        <div className="DataCard__title">{title}</div>
+        <div className="DataCard__data">{data?.toLocaleString()}</div>
+        </div>
+    )
+}
