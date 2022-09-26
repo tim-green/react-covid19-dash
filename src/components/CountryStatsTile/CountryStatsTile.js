d051d52ce087cb173735bf3b4191060d647f0f47
Tim <github@timgreen.ws>
2022-09-26 16:51:45 +1000

    added country stats tile function

---
+++ src/components/CountryStatsTile/CountryStatsTile.js
@@ -0,0 +1,16 @@
+import DataCard from "../shared/DataCard/DataCard";
+import _ from "lodash";
+
+export default function CountryStatsTile({countryStats}){
+    const dataCards = ["NewConfirmed", "TotalConfirmed","NewDeaths", "TotalDeaths"].map((dataAttr) => {
+        return <DataCard key={`countryStats_${dataAttr}`} title={_.startCase(dataAttr).replace("Confirmed", "Case")} data={countryStats[dataAttr]} />;
+        }
+    );
+
+    return(
+        <div className="StatsTile CountryStatsTile">
+            <h2>Stats for {countryStats.Country}</h2>
+            <div className="DataCards">{dataCards}</div>
+        </div>
+    )
+}
